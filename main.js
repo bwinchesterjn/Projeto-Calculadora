c490a2235aa1231a8ef802509fd1ef113bf97976
Bruno <bruno.winchester@icloud.com>
2024-07-31 19:49:57 +0100

    Calculandso media e rodando

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const atividades = [];
 const notas = [];
 const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
 const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"));
 
 let linhas = '';
 
@@ -23,21 +24,20 @@ function adicionaLinha() {
     if (atividades.includes(inputNomeAtividade.value)) {
         alert(`A Atividade: ${inputNomeAtividade.value} Já foi inserida`);
     } else {
+        atividades.push(inputNomeAtividade.value);
+        notas.push(parseFloat(inputNotaAtividade.value));
+        
+        let linha = '<tr>';
+        linha += `<td>${inputNomeAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value}</td>`;
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado }</td>`;
+        linha += '</td>';
 
-    atividades.push(inputNomeAtividade.value);
-    notas.push(parseFloat(inputNotaAtividade.value));
-
-    let linha = '<tr>';
-    linha += `<td>${inputNomeAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value}</td>`;
-    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado }</td>`;
-    linha += '</td>';
-
-    linhas += linha
-
+        linhas += linha
+        }
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
-}
+    }
 
 function atualizaTabela() {
     const corpoTabela = document.querySelector('tbody');
@@ -48,7 +48,7 @@ function atualizaMediaFinal() {
     const mediaFinal = calculaMediaFinal();
 
     document.getElementById('media-final-valor').innerHTML = mediaFinal;
-    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= 7 ? spanAprovado : spanReprovado;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
     
 }
 
